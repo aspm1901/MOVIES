01ba47cd4b7aaa076ee91e35b15e293dd4c6f106
aspm1901 <andrewpella2004@gmail.com>
2024-09-28 17:41:51 -0500

    a

--- JS/modal.js
+++ JS/modal.js
@@ -1,6 +1,3 @@
-// modal.js
-
-// Función para abrir el modal
 function openModal(movie) {
   document.getElementById("modalMovieImage").src = movie.img;
   document.getElementById("movieTitle").innerText = movie.title;
@@ -8,29 +5,24 @@ function openModal(movie) {
   document.getElementById("movieModal").style.display = "block";
 }
 
-// Función para cerrar el modal
 function closeModal() {
   document.getElementById("movieModal").style.display = "none";
 }
 
-// Añadir un evento a cada botón de la película
 const movieCards = document.querySelectorAll(".movie-card");
 movieCards.forEach((card) => {
   card.addEventListener("click", (event) => {
-    // Verificar si el clic fue en el botón de "Favorito"
     if (event.target.classList.contains("favorite-btn")) {
-      return; // No abrir el modal si se hizo clic en "Favorito"
+      return;
     }
 
-    // Crear objeto de película
     const movie = {
       img: card.querySelector(".movie-img").src,
       title: card.querySelector("h3").innerText,
       description: card.querySelector("p").innerText,
     };
-    openModal(movie); // Abrir modal para la película
+    openModal(movie);
   });
 });
 
-// Cerrar el modal al hacer clic fuera de él
 document.getElementById("movieModal").addEventListener("click", closeModal);
